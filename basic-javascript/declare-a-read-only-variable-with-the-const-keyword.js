a9fd43b497ea916005063f89d71860616c83f35b
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2021-12-15 11:50:49 +0300

    feat: declare-a-read-only-variable-with-the-const-keyword

---
+++ basic-javascript/declare-a-read-only-variable-with-the-const-keyword.js
@@ -0,0 +1,14 @@
+// before
+
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+
+
+//after
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
