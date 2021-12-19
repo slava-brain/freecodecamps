d47e75fd58a65ea61d5fb925ffc3a3ac818564b6
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2021-12-19 14:35:56 +0300

    feat: assignment with a returned value

---
+++ basic-javascript/assignment-with-a-returned-value.js
@@ -0,0 +1,20 @@
+/* 
+
+If you'll recall from our discussion of Storing Values with the Assignment Operator,
+everything to the right of the equal sign is resolved before the value is assigned.
+This means we can take the return value of a function and assign it to a variable.
+
+Assume we have pre-defined a function sum which adds two numbers together, then:
+
+ourSum = sum(5, 12);
+will call the sum function, which returns a value of 17 and assigns it to the ourSum variable.
+
+*/
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
