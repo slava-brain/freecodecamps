3ef055fbb7de5e48c7e9e554514af0bd9481de7b
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2021-12-19 15:15:59 +0300

    feat: comparisons with the logical or operator

---
+++ basic-javascript/comparisons-with-the-logical-or-operator.js
@@ -0,0 +1,33 @@
+/* 
+
+The logical or operator (||) returns true if either of the operands is true. Otherwise, it returns false.
+
+The logical or operator is composed of two pipe symbols: (||). This can typically be found between your Backspace and Enter keys.
+
+The pattern below should look familiar from prior waypoints:
+
+if (num > 10) {
+  return "No";
+}
+if (num < 5) {
+  return "No";
+}
+return "Yes";
+will return Yes only if num is between 5 and 10 (5 and 10 included). The same logic can be written as:
+
+if (num > 10 || num < 5) {
+  return "No";
+}
+return "Yes";
+
+*/
+
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  return "Inside";
+}
+
+testLogicalOr(15);
