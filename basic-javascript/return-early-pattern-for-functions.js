c241acaba85dce92c550944559f2dcd08f1b6abd
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2021-12-20 19:57:41 +0300

    feat: return early pattern for functions

---
+++ basic-javascript/return-early-pattern-for-functions.js
@@ -0,0 +1,27 @@
+/* 
+
+When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+Example
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+The above will display the string Hello in the console, and return the string World.
+The string byebye will never display in the console, because the function exits at the return statement.
+
+*/
+
+// Setup
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
